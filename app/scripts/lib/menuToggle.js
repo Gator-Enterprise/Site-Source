da7e4b0322e021caebf5dcf02e7a5c94ce61863c
Brice Shatzer <brice.shatzer@gmail.com>
2017-05-11 14:17:43 -0500

    Added partial for menuToggle, fancybox plugin, & a local copy of jQuery as a fallback

---
+++ app/scripts/lib/menuToggle.js
@@ -0,0 +1,10 @@
+function defineMenuToggleHandler(){
+    $('#menu-toggle').click(function(){
+        $(this).toggleClass('is-active');
+        if( $(this).hasClass('is-active') ){
+            $('.mobile-menu-items').addClass('show');
+        } else {
+            $('.mobile-menu-items').removeClass('show');
+        }
+    });
+}
